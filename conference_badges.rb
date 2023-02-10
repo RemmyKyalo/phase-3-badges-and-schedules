881cf7c5474d7d6f7ec1d90306f35bd11ab5d6c6
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-02-10 19:33:48 +0300

    commit tree 981bc5bb798abc98e3cb3fd8eb64b1dc443b33d9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map  do |name|
+        "Hello, my name is #{name}."
+    end
+
+end
+
+def assign_rooms(array)
+   list= array.map do|name|
+       "Hello, #{name}! You'll be assigned to room #{array.index(name)+1}!" 
+    end
+    
+end
+
+def printer(attendees)
+badge_messages = batch_badge_creator(attendees)
+room_assignments = assign_rooms(attendees)
+
+badge_messages.each { |message| puts message }
+room_assignments.each { |assignment| puts assignment }
+end
